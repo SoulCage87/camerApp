c5b0f839abc9e2dc453a0539fc2d12c5139966de
Samuel Galo <samuelgalo1625@gmail.com>
2024-05-19 15:05:53 -0600

    commit 1

---
+++ src/components/events.tsx
@@ -0,0 +1,5 @@
+export const events = [
+    {id: 1, nombre: 'Google I/O', descripcion: 'Disfruta que Google tiene preparado para ti acerca del mundo web'},
+    {id: 2, nombre: 'Nintendo Direct', descripcion: 'Conoce los nuevos lanzamientos que nintendo tiene preparado para ti'},
+    {id: 3, nombre: 'Sonic Central', descripcion: 'Conoce los lanzamientos del erizo azul dados por el mismisimo Takashi Izuka'}
+]
---
+++ src/screen/Events.tsx
@@ -0,0 +1,17 @@
+import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+
+const Events = () => {
+
+  const [selectEvent, setSelectEvent] = useState(null)
+
+
+
+  return (
+    <View>
+      <Text>Events</Text>
+    </View>
+  )
+}
+
+export default Events
