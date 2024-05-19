b106066a448b1729bf6c03e487cfc5c1cb9cf199
Samuel Galo <samuelgalo1625@gmail.com>
2024-05-19 15:38:05 -0600

    commit
    Mostrar en pantalla los eventos

--- src/screen/Events.tsx
+++ src/screen/Events.tsx
@@ -1,5 +1,6 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native'
 import React, { useState } from 'react'
+import { events } from '../components/events'
 
 const Events = () => {
 
@@ -8,10 +9,27 @@ const Events = () => {
 
 
   return (
-    <View>
-      <Text>Events</Text>
+    <View style={{alignSelf: 'center', marginTop: 250}}>
+       <FlatList  
+        data={events}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <TouchableOpacity onPress={() => setSelectEvent(item)}>
+            <Text style={styles.nameEvento}>{item.nombre}</Text>
+            <Text style={styles.nameEvento}>{item.descripcion}</Text>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  nameEvento: {
+    fontSize: 20,
+    marginVertical: 12,
+    textAlign: 'center'
+  }
+})
 
 export default Events
